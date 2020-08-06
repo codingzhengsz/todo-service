cc35a8d6dc3aae2913988fca1daaa2a5abc88de8
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-08-06 09:11:10 +0800

    change the server port to 8090

--- src/main/java/com/example/todolist/controller/GreetingController.java
+++ src/main/java/com/example/todolist/controller/GreetingController.java
@@ -1,17 +1,21 @@
 package com.example.todolist.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
+
+import java.util.Map;
 
 @Controller
 public class GreetingController {
   @MessageMapping("/hello")
   @SendTo("/topic/greetings")
-  public String greeting(String name) throws Exception {
+  public Map<String, String> greeting(@Payload Map<String, String> message) throws Exception {
     Thread.sleep(1000); // simulated delay
-    return String.format("Hello, " + HtmlUtils.htmlEscape(name) + "!");
+    message.put("timestamp", Long.toString(System.currentTimeMillis()));
+    return message;
   }
 
 }
