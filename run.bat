03c99a84d58d79cfcd5588d30db1b3af848609f7
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-08-06 11:51:57 +0800

    fix the script issue

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 for /f "tokens=5 delims= " %%a in ('netstat -ano ^| findstr "8090" ^| findstr "LISTENING"') do set pid=%%a
-if "%pid%" neq "" if "%pid% neq "0" (taskkill /f /pid %pid%)
+if "%%pid%%" neq "" if "%%pid%% neq "0" (taskkill /f /pid %pid%)
