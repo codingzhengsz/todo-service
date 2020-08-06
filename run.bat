8720d710f43fe0b2896149c3b37f4fa359ab8f07
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-08-06 11:56:45 +0800

    fix the script issue

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 for /f "tokens=5 delims= " %%a in ('netstat -ano ^| findstr "8090" ^| findstr "LISTENING"') do set pid=%%a
 if not "%pid%" == "" (
-  taskkill /PID %pid%
+  taskkill /f /PID %pid%
 ) else (
   rem echo Server is not running.
 )
